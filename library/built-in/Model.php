98251037f7252bb731d12ddbc98a705e31ce2d2e
Václav Franz <vaclav.franz@gmail.com>
2023-09-21 08:15:13 +0200

    model.php: add DB data validation

--- library/built-in/Model.php
+++ library/built-in/Model.php
@@ -27,18 +27,44 @@ class Model {
         return $this->db->get($table_name, $num_rows, $columns);
     }
 
-    public function insertToTable(string $table, array $data_to_insert) {
+    public function insertToTable(string $table, array $data_to_insert, bool $not_null=false) {
+        if ($not_null == false) {
+            $data_to_insert = $this->validateDataForTable($data_to_insert);
+        }
         return $this->db->insert($table, $data_to_insert);
     }
 
     /**
      * @param array $where    ['column_name'=>'value'] -> can add multiple where conditions
      */
-    public function updateTableRow(string $table, array $where, array $data_to_update, string $where_operator='=', string $where_condition='AND'): bool {
+    public function updateTableRow(string $table, array $where, array $data_to_update, string $where_operator='=', string $where_condition='AND', bool $not_null=false): bool {
         foreach ($where as $column=>$value) {
             $this->db->where($column, $value, $where_operator, $where_condition);
         }
+        if ($not_null == false) {
+            $data_to_update = $this->validateDataForTable($data_to_update);
+        }
         return $this->db->update($table, $data_to_update);
+    }
+
+    public function deleteTableRow(string $table, array $where, int $num_of_rows=null, string $where_operator='=', string $where_condition='AND') {
+        foreach ($where as $column=>$value) {
+            $this->db->where($column, $value, $where_operator, $where_condition);
+        }
+        return $this->db->delete($table, $num_of_rows);
+    }
+
+    private function validateDataForTable(array $data_to_insert): array {
+        $result = array();
+        $array_keys = array_keys($data_to_insert);
+        foreach ($array_keys as $key) {
+            if (empty($data_to_insert[$key])) {
+                $result[$key] = NULL;
+            } else {
+                $result[$key] = $data_to_insert[$key];
+            }
+        }
+        return $result;
     }
     
 }
